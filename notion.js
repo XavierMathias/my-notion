89566642647461baa3aa2fa008b2607d7764de98
David Santos <dsdsantos973@gmail.com>
2024-01-15 21:41:52 -0600

    commenting notion

--- notion.js
+++ notion.js
@@ -2,18 +2,19 @@ const {Client} = require("@notionhq/client") // getting client from notion libra
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY}) // creating an instance of a notion
 
+// this creates a page in the notion db
 function createSuggestion({ title }){
-    notion.pages.create({
-        parent:{
+    notion.pages.create({ // creating a page
+        parent:{ // identifying the parent db by database id
             database_id: process.env.NOTION_DB_ID
         },
-        properties: {
-            [process.env.DB_TITLE_ID]: {
-                title: [
+        properties: { // modifying the properties of the page
+            [process.env.DB_TITLE_ID]: { // identifying the page by property id
+                title: [ // property heading
                     {
-                        type: "text",
+                        type: "text", // assigning property type 
                         text: {
-                            content: title
+                            content: title // passing whatever the parameters of the function is assigned
                         }
                     }
                 ]
@@ -22,3 +23,5 @@ function createSuggestion({ title }){
     })
 
 }
+
+createSuggestion( {title: "my first page on the BD"})
